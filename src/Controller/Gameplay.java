70e55a360cebb71cb40fcab0f45d864c44bccdf1
DomianosPizza <damianl.official@gmail.com>
2023-02-02 09:37:00 -0700

    made game "functional"

--- src/Controller/Gameplay.java
+++ src/Controller/Gameplay.java
@@ -5,60 +5,70 @@ import View.Console;
 import View.Display;
 
 public class Gameplay {
-static  BigBoard bigBoard;
-static boolean gameWon = false;
+    static BigBoard bigBoard;
+    static boolean gameWon = false;
+
     public static void GameplayLoop() {
         int turns = 0;
         String[] playerNames = {"player 1", "player 2"};
         char[] playerSymbols = {'X', 'O'};
-        int[] coordinates = new int[] {-1, -1};
+        int[] coordinates = new int[]{-1, -1};
 
 
         //call different functions :)
         bigBoard = Display.InitializeBoards();
 
         do {
-            coordinates = playerTurn(playerNames[turns%2], playerSymbols[turns%2],bigBoard, coordinates);
-            if (checkForWinner(bigBoard.getBoard()[coordinates[0]][coordinates[1]].getBoard())) {
-                fillBoard(bigBoard.getBoard()[coordinates[0]][coordinates[1]].getBoard(), playerSymbols[turns%2]);
-            }
+            coordinates = playerTurn(playerNames[turns % 2], playerSymbols[turns % 2], bigBoard, coordinates);
 
-        }while(!gameWon);
+            if (checkForWinner(bigBoard.getBoard()[coordinates[0]][coordinates[1]].getBoard())) {
+                fillBoard(bigBoard.getBoard()[coordinates[0]][coordinates[1]].getBoard(), playerSymbols[turns % 2]);
+
+            }
+            turns++;
+        } while (!gameWon);
     }
 
     public static int[] playerTurn(String playerName, char playerSymbol, BigBoard board, int[] smallBoard) {
         System.out.println(playerName + ", it's your turn!"); //Tell the player it's their turn
 
         boolean validCoordinate = false;
-        int row = -1;
-        int col = -1;
+        int row = smallBoard[0];
+        int col = smallBoard[1];
+        int smallRow;
+        int smallCol;
+
 
         //Choose small board if coords are -1 -1
         if (smallBoard[0] == -1 && smallBoard[1] == -1) {
+
             do {
-                row = Console.getInteger("Which row would you like to place in?", 1, 3); //Ask them for a row
-                col = Console.getInteger("Which column would you like to place in?", 1, 3); //Ask them for a column
-                if (playingBoard[row][col] == ' ') { //Check if they can place there (not already placed)
+                Display.DisplayBigBoard(board, new int[]{row, col});
+                row = Console.getInteger("Which row would you like to place in?\n", 1, 3) - 1; //Ask them for a row
+                col = Console.getInteger("Which column would you like to place in?\n", 1, 3) - 1; //Ask them for a column
+                if (!checkForFullBoard(board, new int[]{row, col})) { //Check if they can place there (not already placed)
                     validCoordinate = true;
-                    playingBoard[row][col] = playerSymbol;
                 }
-            }while(!validCoordinate);
+                else Console.println("NOT VALID SPOT! PICK AGAIN", Console.RED);
+            } while (!validCoordinate);
+        }
+        validCoordinate = false;
+        char[][] playingBoard = board.getBoard()[row][col].getBoard(); //Find the board the player needs to place on
+        do {
+            Display.DisplayBigBoard(board, new int[]{row, col});
+            smallRow = Console.getInteger("Which row would you like to place in?\n", 1, 3) - 1; //Ask them for a row
+            smallCol = Console.getInteger("Which column would you like to place in?\n", 1, 3) - 1; //Ask them for a column
+            if (playingBoard[smallRow][smallCol] == ' ') { //Check if they can place there (not already placed)
+                validCoordinate = true;
+                playingBoard[smallRow][smallCol] = playerSymbol;
+            }
+        } while (!validCoordinate);
+        if (checkForWinner(bigBoard.getBoard()[row][col].getBoard())) {
+            fillBoard(bigBoard.getBoard()[row][col].getBoard(), playerSymbol);
+
         }
 
-        char[][] playingBoard = board.getBoard()[smallBoard[0]][smallBoard[1]].getBoard(); //Find the board the player needs to place on
-
-        validCoordinate = false;
-
-        do {
-            row = Console.getInteger("Which row would you like to place in?", 1, 3); //Ask them for a row
-            col = Console.getInteger("Which column would you like to place in?", 1, 3); //Ask them for a column
-            if (playingBoard[row][col] == ' ') { //Check if they can place there (not already placed)
-                validCoordinate = true;
-                playingBoard[row][col] = playerSymbol;
-            }
-        } while(!validCoordinate);
-
-        return new int[] {row, col};//return the coordinates the player placed in
+        return new int[]{smallRow, smallCol};//return the coordinates the player placed in
     }
 
     public static boolean checkForWinner(char[][] board) {
@@ -85,15 +95,16 @@ static boolean gameWon = false;
     }
 
     public static boolean checkForFullBoard(BigBoard board, int[] coordinates) {
-        for (int row = 0; row < 3; row++) {
-            for (int smallRow = 0; smallRow < 3; smallRow++) {
-                for (int col = 0; col < 3; col++) {
-                    for (int smallCol = 0; smallCol < 3; smallCol++) {
-                        //Check if the smallboard is full
-                    }
+        for (int smallRow = 0; smallRow < 3; smallRow++) {
+            for (int smallCol = 0; smallCol < 3; smallCol++) {
+                //Check if the smallboard is full
+                if (board.getBoard()[coordinates[0]][coordinates[1]].getBoard()[smallRow][smallCol] == ' ') {
+                    return false;
                 }
             }
         }
+
+        return true;
     }
 
 }
--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,12 +1,8 @@
 package Controller;
 
-import View.BigBoard;
-import View.Console;
-import View.Display;
-
 public class Main {
     public static void main(String[] args) {
-        Display.DisplayBigBoard(Display.InitializeBoards(),new int[]{-1,-1});
+        //Display.DisplayBigBoard(Display.InitializeBoards(),new int[]{-1,-1});
         Gameplay.GameplayLoop();
         System.out.println(Gameplay.checkForWinner(Gameplay.bigBoard.getBoard()[0][0].getBoard()));//OOP
     }
